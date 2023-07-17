b9d2709ebedc6a431aaaf0f6bedfa17b78218c57
Ibrahima <iab.jr10@gmail.com>
2023-07-16 22:03:53 -0400

    Add develop files

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// TODO: Include packages needed for this application
 const fs = require('fs');
 // Allow use of file system (writeFile, readFile, )
 const inquirer = require('inquirer');
 // Allow use of inquirer to ask user for inputs
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
