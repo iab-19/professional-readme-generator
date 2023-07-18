3610b39ce35e8fd76e7caa898a4381a0ec794f8c
Ibrahima <iab.jr10@gmail.com>
2023-07-18 19:36:40 -0400

    Update template.md and index.js files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-// Allow use of file system (writeFile, readFile, )
+// Extend functionality of node.js
+// to allow use of file system (writeFile, readFile, )
 const inquirer = require('inquirer');
 // Allow use of inquirer to ask user for inputs
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['1. Enter the title of your project: ',
+'2. Enter a short description about your project: ',
+'3. Enter installation instructions for your project: ',
+'4. Enter usage information for your project: ',
+'5. Enter contribution guidelines for your project'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
